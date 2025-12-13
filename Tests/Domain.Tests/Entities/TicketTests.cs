8d46e609e7286f8fe5a2f1822fa37d677f8b09f0
Vinicidesu <viniciusmoraeslima@gmail.com>
2025-12-13 08:09:32 -0300

    tests: fodase

--- Tests/Domain.Tests/Entities/TicketTests.cs
+++ Tests/Domain.Tests/Entities/TicketTests.cs
@@ -13,18 +13,17 @@ namespace Domain.Tests.Entities
                 Id = 1,
                 Title = "Test Ticket",
                 Description = "Test Description",
-                CreatedAt = DateTime.Now,
+                CreatedAt = DateTime.UtcNow,
                 CreatedBy = "TestUser",
-                UpdatedAt = DateTime.Now,
+                UpdatedAt = DateTime.UtcNow,
                 UpdatedBy = "TestUser"
             };
             var newStatus = TicketStatus.InProgress;
             var updatedBy = "TestUser2";
             // Act
-            var result = ticket.ChangeStatus(newStatus, updatedBy);
+            ticket.ChangeStatus(newStatus, updatedBy);
             // Assert
             Assert.Equal(newStatus, ticket.Status);
-            Assert.Equal("Status changed to InProgress", result);
             Assert.Equal(updatedBy, ticket.UpdatedBy);
             Assert.True((DateTime.Now - ticket.UpdatedAt).TotalSeconds < 1);
         }
